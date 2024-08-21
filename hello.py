8cd39f81bfb9b8bc07ad6bea22ed002b09191b30
SergioRA1 <sergioranton@gmail.com>
2024-08-21 16:58:11 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello world")
+name = input("What's your name? ") #asks what's the user's name # name is an input
+#print("hello", name, "nice to meet you", end= "")
+# returns `hello (user input) nice to meet you´
+#end = ""-- makes print not create a new line at the end, which it does by default which is interesting
+name = name.strip()#deletes spaces on left and right
+name = name.title()
+
+#:you can join them together
+name = name.strip().title()
+
+#better way of doing that string
+print(f"hello, {name}")
